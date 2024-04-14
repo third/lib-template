abf4f07de0272c7789bfc6a9e6c87a19636ca316
Third Santor <tlsantor@gmail.com>
2024-04-14 14:49:56 -0400

    Publishable version with treeshakeable css

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
@@ -15,6 +17,23 @@ export default defineConfig({
         copyPublicDir: false,
         rollupOptions: {
             external: ['react', 'react/jsx-runtime'],
+            input: Object.fromEntries(
+                glob.sync('lib/**/*.{ts,tsx}').map((file) => [
+                    // The name of the entry point
+                    // lib/nested/foo.ts becomes nested/foo
+                    relative(
+                        'lib',
+                        file.slice(0, file.length - extname(file).length)
+                    ),
+                    // The absolute path to the entry file
+                    // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                    fileURLToPath(new URL(file, import.meta.url)),
+                ])
+            ),
+            output: {
+                assetFileNames: 'assets/[name][extname]',
+                entryFileNames: '[name].js',
+            },
         },
     },
 });
